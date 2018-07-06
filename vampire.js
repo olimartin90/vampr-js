f8667aa008da29daa2bba194976c1ac3dda9cdec
Olivier Martin <olimartin90@gmail.com>
2018-07-06 02:53:17 +0000

    intro trees - vampire - all functions passed except stretch

--- vampire.js
+++ vampire.js
@@ -2,30 +2,41 @@ class Vampire {
   constructor(name, yearConverted) {
     this.name = name;
     this.yearConverted = yearConverted;
-    this.offspring = [];
     this.creator = null;
+    this.offspring = [];
   }
 
   /** Simple tree methods **/
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
+    let numberOfVampire = 0;
+    let currentVampire = this;
 
+    while (currentVampire.creator) {
+      currentVampire = currentVampire.creator;
+      numberOfVampire++;
+    }
+    return numberOfVampire;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    if (this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal) {
+      return true;
+    }
+    return false;
   }
 
   /** Stretch **/
@@ -35,10 +46,9 @@ class Vampire {
   // For example:
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
-  closestCommonAncestor(vampire) {
+  // closestCommonAncestor(vampire) {
 
-  }
+  // }
 }
 
 module.exports = Vampire;
-
